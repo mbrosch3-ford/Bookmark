da895d8c09fbfca907a218f046c7c12a689aa7ca
MarcBrosch <marc.brosch@ford.com>
2021-11-13 11:29:55 +0100

    Bookmarks hardcoded

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 require 'sinatra/reloader'
 
@@ -7,8 +9,9 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/' do
+    @bookmarks = ['http://www.ford.com', 'http://www.youtube.com']
     erb :index
   end
 
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
 end
--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 feature 'Index page' do
   scenario 'it shows the list of bookmarks' do
     visit '/'
-    expect(page).to have_content 'Bookmarks'
+    expect(page).to have_content 'http://www.ford.com'
+    expect(page).to have_content 'http://www.youtube.com'
   end
 end
